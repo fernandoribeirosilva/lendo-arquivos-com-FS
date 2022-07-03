c74bc01fa5165133151ea1cf902ab5e23c62462d
Fernando Ribeiro da Silva <fernando1996ribeirosilva@gmail.com>
2022-07-03 15:54:03 -0300

    implementado função que vai entra em cada pasta e buscar somente os arquivos .html

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 const fs = require('fs').promises;
 const path = require('path');
 
-
 async function readdir(rootDir) {
    rootDir = rootDir || path.resolve(__dirname);
    const files = await fs.readdir(rootDir);
-
-
+   walk(files, rootDir);
 }
 
 async function walk(files, rootDir) {
    for (let file of files) {
+      const fileFullPath = path.resolve(rootDir, file);
+      const stats = await fs.stat(fileFullPath);
+
+      // não vai pegar os arquivos .git e a pasta node_modules
+      if (/\.git/g.test(fileFullPath)) continue;
+      if (/node_modules/g.test(fileFullPath)) continue;
+
+      if (stats.isDirectory()) {
+         readdir(fileFullPath);
+         continue;
+      }
+
+      // vai pegar apenas os arquivos .html
+      if (!/\.html$/g.test(fileFullPath)) continue;
       console.log(fileFullPath);
    }
 }
-readdir('D:\\Documentos\\#Curso-de-JavaScript-e-TypeScript\\projeos\\');
+readdir('/media/luizotavio/Externo/JS/');
