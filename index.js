b9d9d0aec196c6c7890cebe4e32db3f5769f2eb3
Fernando Ribeiro da Silva <fernando1996ribeirosilva@gmail.com>
2022-07-03 15:55:19 -0300

    implementado função que vai entra em cada pasta e buscar somente os arquivos .html

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('path');
 async function readdir(rootDir) {
    rootDir = rootDir || path.resolve(__dirname);
    const files = await fs.readdir(rootDir);
+
    walk(files, rootDir);
 }
 
@@ -26,4 +27,4 @@ async function walk(files, rootDir) {
       console.log(fileFullPath);
    }
 }
-readdir('/media/luizotavio/Externo/JS/');
+readdir('D:\\Documentos\\#Curso-de-JavaScript-e-TypeScript\\projeos');
