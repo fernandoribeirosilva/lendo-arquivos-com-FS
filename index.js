492a50adcd9ab729204812881a2b305a6a4b5e8f
Fernando Ribeiro da Silva <fernando1996ribeirosilva@gmail.com>
2022-07-03 14:51:00 -0300

    primeiro commit

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
+const fs = require('fs').promises;
+const path = require('path');
 
 
+(async function readdir(rootDir) {
+   rootDir = rootDir || path.resolve(__dirname);
+   const files = await fs.readdir(rootDir);
+
+   walk(files);
+})('D:\\Documentos\\#Curso-de-JavaScript-e-TypeScript\\projeos\\');
+
+function walk(files) {
+   for (let file of files) {
+      console.log(file);
+   }
+}
 // readdir()
 
